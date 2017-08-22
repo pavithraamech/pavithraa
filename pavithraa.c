b499c687fb0100d89452f13f67699634ec60ea3d
pavithraamech <30343599+pavithraamech@users.noreply.github.com>
2017-08-22 06:33:17 -0400

    Create pavithraa.c

---
+++ pavithraa.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+printf("Enter the value of a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c));
+{
+printf("A is a biggest number");
+}
+elseif(b>c)
+{
+prinf("B is a biggest number");
+}
+else
+{
+printf("C is a biggest number");
+}
+}
